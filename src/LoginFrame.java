178d1ccf933d1aed7520496741cbb1fa14809ca7
Sepehr Tavakoli <9831111@ceit.local>
2020-07-22 18:31:05 +0430

    Add initLoginFrame method.

--- src/LoginFrame.java
+++ src/LoginFrame.java
@@ -28,10 +28,42 @@ public class LoginFrame extends JFrame {
         setLayout(new BorderLayout());
         setBounds(700, 250, 320, 515);
 
-//        initLoginFrame();
+       initLoginFrame();
 //        initRemindMe();
 //        initInformationPanel();
         setVisible(true);
 
     }
+    public void initLoginFrame() {
+
+        tankPhoto = new ImageIcon("gifFiles/gif.gif");
+        JLabel tankPhotoLabel = new JLabel(tankPhoto);
+        add(tankPhotoLabel, BorderLayout.NORTH);
+
+        userName = new JTextField();
+        userName.setBorder(new TitledBorder("Username :"));
+        userName.setBackground(Color.lightGray);
+        passwordField = new JPasswordField("");
+        passwordField.setBorder(new TitledBorder("Password :"));
+        passwordField.setBackground(Color.lightGray);
+        remindMe = new JCheckBox("Remind Me");
+        remindMe.setBackground(Color.lightGray);
+
+        buttonPanel = new JPanel();
+        buttonPanel.setBorder(new LineBorder(Color.DARK_GRAY));
+        signIn = new JButton(" Sign in ");
+        signUp = new JButton(" Sign up ");
+        signIn.setFont(new Font("Marker Felt", Font.BOLD, 15));
+        signUp.setFont(new Font("Marker Felt", Font.BOLD, 15));
+        buttonPanel.add(signIn);
+        buttonPanel.add(signUp);
+
+        add(buttonPanel, BorderLayout.SOUTH);
+
+//        signUp.addActionListener(new SignUpAction());
+//        signIn.addActionListener(new SignInAction());
+
+
+    }
+
 }
