5f7eaf61b2b381811a0d44ac5f77c2d0143d686f
Sepehr Tavakoli <9831111@ceit.local>
2020-07-22 18:50:25 +0430

    Add initUserInformationPanel method.

--- src/SettingFrame.java
+++ src/SettingFrame.java
@@ -1,5 +1,9 @@
 import javax.swing.*;
+import javax.swing.border.LineBorder;
 import java.awt.*;
+import java.io.File;
+import java.io.FileReader;
+import java.util.Scanner;
 
 public class SettingFrame extends JFrame {
 
@@ -23,5 +27,47 @@ public class SettingFrame extends JFrame {
 
         setVisible(true);
     }
+    public void initUserInformationPanel() {
+
+        userInformationPanel = new JPanel();
+        userInformationPanel.setBackground(Color.darkGray);
+        userInformationPanel.setLayout(new GridLayout(5, 2));
+
+
+        File accounts = new File("Accounts.txt");
+        try (Scanner scanner = new Scanner(new FileReader(accounts))) {
+            String account;
+            while (scanner.hasNextLine()) {
+                account = scanner.nextLine() + "\n";
+                String[] strings = account.split(" ");
+                if (strings[0].equals(Controller.userName)) {
+
+                    textAndLabel("pictures/user.png", Controller.userName);
+
+                    textAndLabel("pictures/playedTime.png", strings[3]);
+
+                    textAndLabel("pictures/offlineGame.png", "Win = " + strings[4] + "      |     Lost = " + strings[5]);
+
+                    textAndLabel("pictures/onlineGame.png", "Win = " + strings[6] + "      |     Lost = " + strings[7]);
+
+                }
+            }
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+
+        ImageIcon userTankIcon = new ImageIcon("pictures/userTank.png");
+        tankLabel = new JLabel(userTankIcon);
+        userInformationPanel.add(tankLabel);
+        tank = new ImageIcon("pictures/icon.png");
+        tankPhoto = new JLabel(tank);
+        tankPhoto.setBorder(new LineBorder(Color.GRAY, 2));
+        userInformationPanel.add(tankPhoto);
+
+        add(userInformationPanel);
+
+
+    }
+
 
 }
