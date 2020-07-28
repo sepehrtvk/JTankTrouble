11d027b7568bf99b87789787add261092aa31483
nargessalehi98 <47316389+nargessalehi98@users.noreply.github.com>
2020-07-28 14:23:56 +0430

    Add new field to controller.

--- src/Controller.java
+++ src/Controller.java
@@ -1,3 +1,12 @@
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.util.ArrayList;
+
 public class Controller {
     public static String userName;
+    public static ArrayList<Wall> walls;
+    public static Graphics2D g2d;
+    public static BufferedImage taken;
+    public static int row;
+    public static int col;
 }
