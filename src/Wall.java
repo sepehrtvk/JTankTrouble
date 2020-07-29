e425521088ecfd14e48b8b95b8974294b8345c12
nargessalehi98 <47316389+nargessalehi98@users.noreply.github.com>
2020-07-29 10:34:15 +0430

    Add new Update.

--- src/Wall.java
+++ src/Wall.java
@@ -4,16 +4,14 @@ public class Wall extends Rectangle {
 
     private int x, y, width, height;
     private Graphics2D g2d;
-    private int verOrHor;
 
-    public Wall(int x, int y, int width, int height, Graphics2D g2d,int verOrHor) {
+    public Wall(int x, int y, int width, int height, Graphics2D g2d) {
 
         this.x = x;
         this.y = y;
         this.height = height;
         this.width = width;
         this.g2d = g2d;
-        this.verOrHor=verOrHor;
 
         g2d.fillRect(x, y, width, height);
     }
@@ -32,5 +30,10 @@ public class Wall extends Rectangle {
 
     public double getHeight() {
         return height;
+    }
+
+    @Override
+    public String toString() {
+        return x+" "+y;
     }
 }
