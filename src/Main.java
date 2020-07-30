230e27053388f94d100d9e962b6524e10cd5e8be
nargessalehi98 <47316389+nargessalehi98@users.noreply.github.com>
2020-07-30 20:12:45 +0430

    Add update main.

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,11 @@
+import javax.swing.*;
+import java.awt.*;
+import java.io.IOException;
+
 public class Main {
 
     public static void main(String[] args) {
-        new LoginFrame();
+        //new LoginFrame();
         //new MainFrame();
         //new SettingFrame();
         //new OnlineModeFrame();
@@ -10,5 +14,35 @@ public class Main {
 //        jFrame.setBounds(500,500,700,400);
 //        jFrame.add(new Map());
 //        jFrame.setVisible(true);
+
+
+        // Initialize the global thread-pool
+        ThreadPool.init();
+
+        // Show the game menu ...
+
+        // After the player clicks 'PLAY' ...
+        EventQueue.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                GameFrame frame = null;
+                try {
+                    frame = new GameFrame("JTankTrouble");
+                } catch (AWTException | IOException e) {
+                    e.printStackTrace();
+                }
+                frame.setLocationRelativeTo(null); // put frame at center of screen
+                frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+                frame.setVisible(true);
+                frame.initBufferStrategy();
+                // Create and execute the game-loop
+                GameLoop game = new GameLoop(frame);
+                game.init();
+                ThreadPool.execute(game);
+                // and the game starts ...
+            }
+        });
     }
+
+
 }
