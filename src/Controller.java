eae747f789d731d04bdf9a1dcc637302c900a69f
nargessalehi98 <47316389+nargessalehi98@users.noreply.github.com>
2020-08-02 19:53:00 +0430

    Add new Field.

--- src/Controller.java
+++ src/Controller.java
@@ -4,11 +4,12 @@ import java.util.ArrayList;
 
 public class Controller {
     public static String userName;
-    public static ArrayList<Wall> walls;
     public static Graphics2D g2d;
     public static BufferedImage taken;
     public static int row;
     public static int col;
+    public static ArrayList<Wall> walls;
     public static  ArrayList<Prize> prizes;
+    public static ArrayList<Tank> tanks;
     public static boolean getPrize;
 }
