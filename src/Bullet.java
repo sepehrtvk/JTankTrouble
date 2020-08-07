2d5750087173fe393c58c53dfadfb4770f370e74
nargessalehi98 <47316389+nargessalehi98@users.noreply.github.com>
2020-08-07 13:52:36 +0430

    Add javaDoc.

--- src/Bullet.java
+++ src/Bullet.java
@@ -1,15 +1,29 @@
-import java.awt.geom.Ellipse2D;
-import java.awt.image.BufferedImage;
-
+/**
+ * this class keeps bullet info
+ * @author narges salehi & sepehr tavakoli
+ * @version 1.1
+ * @since July 21 2020
+ */
 public class Bullet {
-
+    //x of bullet
     int x;
+    //y of bullet
     int y;
+    //rotate amount of bullet
     double rotateAmountBullet;
+    //damage of bullet
     int damage;
+    //existence of bullet
     boolean alive;
 
 
+    /**
+     * create a new bullet with given value
+     * @param x of bullet
+     * @param y of bullet
+     * @param rotateAmountBullet of bullet
+     * @param damage of bullet
+     */
     public Bullet(int x, int y, double rotateAmountBullet, int damage) {
         this.x = x;
         this.y = y;
@@ -17,6 +31,4 @@ public class Bullet {
         this.damage = damage;
         alive=true;
     }
-
-
 }
