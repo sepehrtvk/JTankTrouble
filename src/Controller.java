09a1025c01766324e9235a9a5fad29ef9706bdec
nargessalehi98 <47316389+nargessalehi98@users.noreply.github.com>
2020-08-07 13:53:28 +0430

    Add javaDoc.

--- src/Controller.java
+++ src/Controller.java
@@ -2,17 +2,28 @@ import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.util.ArrayList;
 
+/**
+ * this class link between classes and their fields
+ * @author narges salehi & sepehr tavakoli
+ * @version 1.1
+ * @since  @since July 21 2020
+ */
 public class Controller {
+    //redefine some variable as static
     public static String userName;
     public static Graphics2D g2d;
     public static BufferedImage taken;
     public static int row;
     public static int col;
+    public static int realMapSize;
     public static ArrayList<Wall> walls;
-    public static  ArrayList<Prize> prizes;
+    public static ArrayList<Prize> prizes;
     public static ArrayList<Tank> tanks;
+    public static  ArrayList<Integer> prizeLoc;
     public static boolean getPrize;
-    public static long renderCount;
-    public static long renderCountLimit;
-
+    public static int bulletSpeed;
+    public static boolean laser;
+    public static int bulletDamage;
+    public static int tankHealth;
+    public static int wallsHealth;
 }
